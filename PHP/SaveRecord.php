cdf8d17d7308dc3821798ff7b5cf664a548757a5
Wason024 <86592001+Wason024@users.noreply.github.com>
2021-06-28 18:18:06 +0800

    Add files via upload

---
+++ PHP/SaveRecord.php
@@ -0,0 +1,24 @@
+<?php
+if (!isset($_SERVER['PATH_INFO']) or $_SERVER['PATH_INFO']=='/') {
+			return;
+		}
+
+		$this->urlSegments = explode('/', $_SERVER['PATH_INFO']);
+		 
+		array_shift($this->urlSegments);
+		$cutchar = explode("/",$_SERVER['PATH_INFO']);
+			
+		$serviceProviderName = array_shift($this->urlSegments);
+		$serviceProviderFilename = $serviceProviderName;
+		include "database.php";
+		$con = mysqli_connect($dbaddress,$dbaccount,$dbpassword,"digimktg_base",$dbport);
+		$sql= "INSERT INTO `FortuneELuckyDraw`( `times`, `value`, `date_time`) VALUES ('$cutchar[2]','$cutchar[3]','$cutchar[4]')";
+		$sqlcheck = mysqli_query($con, $sqlcheck);	
+		
+		$rec = $sqlcheck->fetch_array();
+	
+		echo 1;
+		
+	$con->close();
+}
+?>
---
+++ PHP/database.php
@@ -0,0 +1,7 @@
+<?php
+	
+	$dbaddress = "202.74.40.175";
+	$dbaccount = "digimktg_user";
+	$dbpassword = "=&Y~Ruw=7ws3";
+	$dbport = "3306";
+?>
